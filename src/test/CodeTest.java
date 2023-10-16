0016d3129448cee0c02f62ce26579644bf9ef5ec
IsaacCastelo <isaacpxl8@gmail.com>
2023-10-16 13:54:14 -0700

    Creacion de proyecto
    .

---
+++ src/test/CodeTest.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ *
+ * @author Isaac Castelo Valenzuela
+ * ID - 216823
+ */
+public class CodeTest {
+
+    public static void main(String[] args) {
+        // Clase para testear código necesario.
+        System.out.println("simple print");
+        System.out.println("este es otro test");
+    }
+
+}
