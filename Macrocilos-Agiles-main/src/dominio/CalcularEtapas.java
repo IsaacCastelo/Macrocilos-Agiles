90ef8df18dccc8bffd9418e4409de9eed9a1a47d
JonatanOros <95501705+JonatanOros@users.noreply.github.com>
2023-11-12 15:42:28 -0700

    Add files via upload

---
+++ Macrocilos-Agiles-main/src/dominio/CalcularEtapas.java
@@ -0,0 +1,93 @@
+
+package dominio;
+
+/**
+ *
+ * @author Kevin
+ */
+public class CalcularEtapas {
+    public int id,semanas, ins;
+    public float min, max, prom, volumenEtapa;
+    public String tipoEtapa;
+
+    public CalcularEtapas() {
+    }
+
+    public CalcularEtapas(int id, int semanas, int ins, float min, float max, float prom, float volumenEtapa, String tipoEtapa) {
+        this.id = id;
+        this.semanas = semanas;
+        this.ins = ins;
+        this.min = min;
+        this.max = max;
+        this.prom = prom;
+        this.volumenEtapa = volumenEtapa;
+        this.tipoEtapa = tipoEtapa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSemanas() {
+        return semanas;
+    }
+
+    public void setSemanas(int semanas) {
+        this.semanas = semanas;
+    }
+
+    public int getIns() {
+        return ins;
+    }
+
+    public void setIns(int ins) {
+        this.ins = ins;
+    }
+
+    public float getMin() {
+        return min;
+    }
+
+    public void setMin(float min) {
+        this.min = min;
+    }
+
+    public float getMax() {
+        return max;
+    }
+
+    public void setMax(float max) {
+        this.max = max;
+    }
+
+    public float getProm() {
+        return prom;
+    }
+
+    public void setProm(float prom) {
+        this.prom = prom;
+    }
+
+    public float getVolumenEtapa() {
+        return volumenEtapa;
+    }
+
+    public void setVolumenEtapa(float volumenEtapa) {
+        this.volumenEtapa = volumenEtapa;
+    }
+
+    public String getTipoEtapa() {
+        return tipoEtapa;
+    }
+
+    public void setTipoEtapa(String tipoEtapa) {
+        this.tipoEtapa = tipoEtapa;
+    }
+    
+    
+    
+}
---
+++ Macrocilos-Agiles-main/test/test/CasosPruebasTest.java
@@ -0,0 +1,47 @@
+
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author
+ */
+public class CasosPruebasTest {
+
+    @Test
+    public void testValidarEntradaDeDatos() {
+        
+    }
+
+    @Test
+    public void testDefinirEtapas() {
+    }
+
+    @Test
+    public void testMostrarVolumenTotal() {
+    }
+
+    @Test
+    public void testLogicaCalculoVolumen() {
+    }
+
+    @Test
+    public void testManejoExcepciones() {
+    }
+
+    @Test
+    public void testAlmacenamientoBaseDatos() {
+    }
+
+    @Test
+    public void testIntegridadConsistenciaDatos() {
+    }
+    
+    
+}
